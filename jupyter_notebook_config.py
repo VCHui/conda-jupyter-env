9c1c0291399d2a985f9d1f803b5231698350b149
Victor Hui <VCHui@users.noreply.github.com>
2022-06-20 16:54:30 +0100

    simple jupyter_notebook_config.py

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,7 @@
+import os
+c.NotebookApp.certfile = os.path.abspath('jupyter.pem')
+c.NotebookApp.keyfile = os.path.abspath('jupyter.pem')
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.notebook_dir = os.path.abspath('ipynb')
+c.NotebookApp.open_browser = False
+c.NotebookApp.password = ""
