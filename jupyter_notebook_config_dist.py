7123a1310f78a128829e1debc2466e63842d9967
Victor Hui <VCHui@users.noreply.github.com>
2024-01-31 12:58:05 +0000

    jupyter_notebook_config_dist.py additional configs

--- jupyter_notebook_config_dist.py
+++ jupyter_notebook_config_dist.py
@@ -1,4 +1,9 @@
-import os
+import sys, os
+c = get_config()  #noqa
+c.NotebookApp.allow_origin = '*'
+c.NotebookApp.allow_remote_access = True
+c.NotebookApp.base_url = '/jupyter'
+# c.NotebookApp.login_handler_class = 'notebook.auth.login.LoginHandler'
 c.NotebookApp.certfile = os.path.abspath('jupyter.pem')
 c.NotebookApp.keyfile = os.path.abspath('jupyter.pem')
 c.NotebookApp.ip = '0.0.0.0'
@@ -6,3 +11,8 @@ c.NotebookApp.notebook_dir = os.path.abspath('ipynb')
 c.NotebookApp.open_browser = False
 c.NotebookApp.password = ""
 c.NotebookApp.token = ""
+c.NotebookApp.port = 8888
+c.NotebookApp.quit_button = True
+c.NotebookApp.terminals_enabled = False
+c.NotebookApp.trust_xheaders = True
+c.ContentsManager.hide_globs = ['__pycache__', '*.pyc', '*.pyo', '.DS_Store', '*.so', '*.dylib', '*~', '*.py']
